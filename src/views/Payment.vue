03b02840377df338af2b28ada3204e78d10aa1b4
Achilles45 <achillesusuoyibo7@gmail.com>
2020-05-27 14:14:34 -0700

    Changed account details

--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -68,16 +68,15 @@
                          <p>Your capital and returns would be paid back after a minimum of 4 days after your payment has been confirmed.</p>
                      </div>
                   </div> -->
-                  <!-- <div class="summary__card four pt-4">
+                  <div class="summary__card four pt-4">
                      <i class="fa fa-home"></i>
                      <div class="content pl-2">
-                         <h6>Bank Accounts</h6>
-                         <h6>Bitcoin Payment</h6>
+                         <h6>Bank Name: {{ bank_name}}</h6>
                          <hr>
-                         <h6>Wallet Address: {{ wallet_address }}</h6>
-                         <h6>Account Name:  Usuoyibo Achilles Ewomamena</h6>
+                         <h6>Account Number:  {{ account_number }}</h6>
+                         <h6>Account Name:  {{ account_name }}</h6>
                      </div>
-                  </div> -->
+                  </div>
               </div>
               </div>
               <hr>
