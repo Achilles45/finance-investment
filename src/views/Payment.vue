89eb5f865b9173e67ceed23a5b42a909f24cf26e
Achilles45 <achillesusuoyibo7@gmail.com>
2020-07-09 11:22:40 +0100

    Fixed account details

--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -56,10 +56,10 @@
                      <!-- <i class="fa fa-home"></i> -->
                      <div class="content pl-4">
                          <!-- <h6>Bank Accounts</h6> -->
-                        <h6>Bank Name: {{ bank_name}}</h6>
+                        <h6>Bank Name: Zenith Bank PLC</h6>
                          <hr>
-                         <h6>Account Number:  {{ account_number }}</h6>
-                         <h6>Account Name:  {{ account_name }}</h6>
+                         <h6>Account Number:  2282941453</h6>
+                         <h6>Account Name:  UBREGWO LUCKY OGHENEKENO</h6>
                      </div>
                   </div>
                     <!-- <div class="summary__card three pt-4">
